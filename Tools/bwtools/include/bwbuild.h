059189abd6bf11bdea94eba40c494b8f532106d3
Dracnea <dracnea@proton.me>
2024-07-08 23:08:57 -0400

    Add bwtools copy and instructions

---
+++ Tools/bwtools/include/bwbuild.h
@@ -0,0 +1,52 @@
+/**
+ *
+ * Desc:	git repo and package version include file
+ *
+ * Intended to be a template for include/bwbuild.h
+ * by running "python tools/scripts/bwgitv.py --gitdir=$(PROJECTDIR) hil/bwbuild.repo include/bwbuild.h"
+ *
+ *  Provided by
+ *  -----------
+ *      BittWare, Inc.
+ *      45 South Main St. Suite L100
+ *      Concord, NH 03301
+ *      Ph:     (603) 226 0404
+ *      Fax:    (603) 226 6667
+ *      WWW:    http://www.bittware.com
+ *      Email:  support@bittware.com
+ *
+ * Copyright (C) 2019, BittWare, Inc.
+ *
+ * License:
+ * This source code is provided to you (the Licensee) under
+ * license by BittWare, a Molex Company. To view or use this
+ * source code, the Licensee must accept a Software License
+ * Agreement (viewable at developer.bittware.com), which is
+ * commonly provided as a click-through license agreement. The
+ * terms of the Software License Agreement govern all use and
+ * distribution of this file unless an alternative superseding
+ * license has been executed with BittWare. This source code
+ * and its derivatives may not be distributed to third parties
+ * in source code form. Software including or derived from this
+ * source code, including derivative works thereof created by
+ * Licensee, may be distributed to third parties with BITTWARE
+ * hardware only and in executable form only.
+ * 
+ * The click-thorough license is available here:
+ * https://developer.bittware.com/software_license.txt
+ * 
+**/
+
+#ifndef BWBUILD_H_
+#define BWBUILD_H_
+
+#define BWBUILD_YR 2020
+#define BWBUILD_RNUM 1L
+#define BWBUILD_DYR 149
+#define BWBUILD_SECD 68658.111811
+#define BWBUILD_STR "2020.1L"
+#define BWBUILD_LSTR "2020.1L.149.68658.111811"
+#define BWBUILD_BUH "ce6918833bfc3d7e40267f2e2002629caadfb8dd"
+#define BWBUILD_BN "2020.1L"
+
+#endif
