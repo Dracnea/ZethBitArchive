77993e4ebd05d1bbb5a4f9ebe1385e960624516c
Dracnea <dracnea@proton.me>
2024-07-18 16:02:09 -0400

    Add K1 Miners Update Nimiq Scripts Add .dll Files Update Documentation

--- Scripts/Nimiq/startZethNimiq.sh
+++ Scripts/Nimiq/startZethNimiq.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-pool=stratum+tcp://nimiq.icemining.ca:2053
-wallet=NQ59NTCV20L7GU6P6U325SMKA0SEJ12N6YFP
+pool=wss://nimiq.icemining.ca:2053
+wallet=NQ92K9NXS6MSRE4CDR176GFAQN3B2Y5X6328
 rig_name=Dracnea_CVP13
 core_clock=300
-
-complexWallet=$wallet.$rig_name
 
 if ! dpkg -l | grep -q "libftdi1-2"; then
 	echo "Installing libftdi1-2..."
     sudo apt-get install libftdi1-2 -y
 fi
 
-/ZethMultiMiner/ZetheronMiners/Nimiq/FPGAMinerNIM-Linux-v10 -o $pool -u $complexWallet -t $core_clock
+/ZethMultiMiner/ZetheronMiners/Nimiq/FPGAMinerNIM-Linux-v10 -o $pool -u $wallet -t $core_clock -p $rig_name --max-temp=80
